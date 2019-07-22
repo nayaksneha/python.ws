fa8e7c89d99e65caae9890a720a1cb59d29a3817
Sneha Nayak <nayaksneha668@gmail.com>
2019-07-22 15:02:01 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,7 @@
+'''Write a program to accept 2 numbers “m” and “n” from the user; determine the value of mn without using predefined functions.'''
+
+m = int(input("enter the number"))
+n = int(input("enter the number"))
+
+num  = m**n 
+print(f"the number is {num}")
